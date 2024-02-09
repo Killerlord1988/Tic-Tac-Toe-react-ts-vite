bd5725ba5109329c5d52ef345db0d0308386d302
Killerlord1988 <killerlord1988@gmail.com>
2024-02-09 16:19:34 +0400

    add App tic-tac-toe

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/*eslint no-undef: "error"*/
+const defaultTheme = require("tailwindcss/defaultTheme")
+
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        display: ["Lobster", ...defaultTheme.fontFamily.sans]
+      }
+    },
+  },
+  plugins: [],
+}
+/*eslint-env node*/
